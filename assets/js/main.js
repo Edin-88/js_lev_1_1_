d115cc7a56d66ed968947e125027fff763a57baf
Your Name <selmanovic.edin1@gmail.com>
2023-01-03 13:32:51 +0100

    finished

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+let a = 12
+let b = a * 3
+
+console.log(b)
+
+document.write(b)
+
+console.log(document.URL);
+
+console.log("Anton");
+
+let firstName = "Anton"
+console.log(firstName)
+
+let age = 28
+console.log(age);
+
+let job = "trainer"
+console.log(job);
+
+let married = true
+console.log(married);
+
+console.log(firstName  + " ist " + age + " Jahre alt unf von Beruf " + job);
